b73bfcb6bb6a1969a021d345225fb7fc9f24c17d
impiyushs <pgsupe@gmail.com>
2017-05-08 15:11:39 -0500

    Create Solution.java

---
+++ Algorithm/Warmup/Staircase/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+         String str="#";
+    for (int i=0;i<n;i++)
+        { 
+            for(int j=1;j<=n;j++)
+                {
+                if(i+j<n)
+                    {System.out.print(" ");}
+                else{System.out.print("#");}
+            }
+        System.out.println();
+    }
+}
+}
