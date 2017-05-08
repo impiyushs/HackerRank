c246d54512831312d33c2e2f9943b937aaaa8404
impiyushs <pgsupe@gmail.com>
2017-05-08 15:10:45 -0500

    Create Solution.java

---
+++ Algorithm/Warmup/Plus Minus/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        float a=0,b=0,c=0;
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+            if(arr[arr_i]<0)
+                {a++;}
+            else if(arr[arr_i]==0)
+                {b++;}
+            else if(arr[arr_i]>0)
+                {c++;}
+        }
+        System.out.println((c/n) + "\n" + (a/n) + "\n" + (b/n));
+    }
+}
