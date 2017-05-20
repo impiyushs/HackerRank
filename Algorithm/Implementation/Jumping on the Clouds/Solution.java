a51345754d53976aa0e8859e06f0152b7b1b05b5
impiyushs <pgsupe@gmail.com>
2017-05-20 01:32:11 -0500

    Create Solution.java

---
+++ Algorithm/Implementation/Jumping on the Clouds/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c[] = new int[n];
+        for(int c_i=0; c_i < n; c_i++){
+            c[c_i] = in.nextInt();
+        }
+        int i=0,count=0;
+        while(i<n-1)
+            {
+            if(i==n-2)
+            {count++;
+             break;}
+            if(c[i+2]==0)
+            {count++;
+             i=i+2;}
+            else if(c[i+1]==0)
+                {
+                count++;
+                i=i+1;
+            } 
+        }
+        System.out.println(count);
+    }
+}
